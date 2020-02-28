f4dcee4b7573928b2d5ddb8d71be1d0619e91c98
Dhosio <danielgithiomi@gmail.com>
2020-02-28 07:33:50 +0300

    this will add the navigation pane at the top of the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 function sizeShow() {
-   $(".sizec").slideToggle(1000);
+   $(".sizec").slideToggle(1500);
 }
 
 function crustShow() {
-   $(".crustc").slideToggle(1000);
+   $(".crustc").slideToggle(1500);
 }
 
 function toppingsShow() {
-   $(".toppingsc").slideToggle(1000);
+   $(".toppingsc").slideToggle(1500);
 }
