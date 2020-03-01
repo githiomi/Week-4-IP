726c3240c3ac85547cccbb3e33c400d31a22ad72
Dhosio <danielgithiomi@gmail.com>
2020-03-02 01:19:02 +0300

    add to cart button added that appends selected item to checkout column

--- js/scripts.js
+++ js/scripts.js
@@ -21,14 +21,90 @@ function toppingsShow() {
    $(".toppingsc").slideToggle(1500);
 }
 
-function sizeOrder(){
-   var sSize = parseInt($(".pizzaSize").val());
-   var size =  $("#medium").val();
-   var size = $("#large").val();
-   
-   console.log(sSize);
+function sizeOrder1() {
+   $(".pizzasizes").show(500);
 
-   // var smallSize = new Order(pizzaSize);
-   
-   // $("ul.ordersView").text("Size: Small Pizza. (Ksh.400/=)" + smallSize.pizzaSize);
+   var size = $("#small").val();
+
+   $(".sizesView").append( size + "<br>");
+}
+function sizeOrder2() {
+   $(".pizzasizes").show(500);
+
+   var size = $("#medium").val();
+
+   $(".sizesView").append(size + "<br>");
+}
+function sizeOrder3() {
+   $(".pizzasizes").show(500);
+
+   var size = $("#large").val();
+
+   $(".sizesView").append(size+ "<br>");
+}
+
+function crustOrder1() {
+   $(".pizzacrusts").show(500);
+
+   var crusts = $("#crispy").val();
+
+   $(".crustsView").append(crusts+ "<br>");
+}
+function crustOrder2() {
+   $(".pizzacrusts").show(500);
+
+   var crusts = $("#stuffed").val();
+
+   $(".crustsView").append(crusts+ "<br>");
+}
+function crustOrder3() {
+   $(".pizzacrusts").show(500);
+
+   var crusts = $("#gluttenfree").val();
+
+   $(".crustsView").append(crusts+ "<br>");
+}
+
+
+function toppingsOrder1() {
+   $(".pizzatoppings").show(500);
+
+   var toppings = $("#tomatoes").val();
+
+   $(".toppingsView").append(toppings+ "<br>");
+}
+function toppingsOrder2() {
+   $(".pizzatoppings").show(500);
+
+   var toppings = $("#cheese").val();
+
+   $(".toppingsView").append(toppings+ "<br>");
+}
+function toppingsOrder3() {
+   $(".pizzatoppings").show(500);
+
+   var toppings = $("#pineapples").val();
+
+   $(".toppingsView").append(toppings+ "<br>");
+}
+function toppingsOrder4() {
+   $(".pizzatoppings").show(500);
+
+   var toppings = $("#greenPepper").val();
+
+   $(".toppingsView").append(toppings+ "<br>");
+}
+function toppingsOrder5() {
+   $(".pizzatoppings").show(500);
+
+   var toppings = $("#pepperoni").val();
+
+   $(".toppingsView").append(toppings+ "<br>");
+}
+function toppingsOrder6() {
+   $(".pizzatoppings").show(500);
+
+   var toppings = $("#mushrooms").val();
+
+   $(".toppingsView").append(toppings+ "<br>");
 }
