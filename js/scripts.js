06707c6346e02098bbd7a3ab84739c674682b5d2
Dhosio <danielgithiomi@gmail.com>
2020-03-02 10:23:33 +0300

    add the Readme file

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-function Order () {
+function Orderprice () {
    this.total = [];
    this.cost = 0;
  }
 
-function Order (pizzaSize, pizzaCrust, pizzaToppings){
+function Singleorder (pizzaSize, pizzaCrust, pizzaToppings){
    this.pizzaSize = pizzaSize;
    this.pizzaCrust = pizzaCrust;
    this.pizzaToppings = pizzaToppings;
@@ -12,99 +12,117 @@ function Order (pizzaSize, pizzaCrust, pizzaToppings){
 function sizeShow() {
    $(".sizec").slideToggle(1500);
 }
-
 function crustShow() {
    $(".crustc").slideToggle(1500);
 }
-
 function toppingsShow() {
    $(".toppingsc").slideToggle(1500);
 }
 
+$(document).ready(function() {
+        event.preventDefault();
+
+         allCrusts = [];
+         allToppings = [];
+
 function sizeOrder1() {
    $(".pizzasizes").show(500);
 
-   var size = $("#small").val();
+   var size = parseInt($("#small").val());
 
-   $(".sizesView").text(size);
+   $(".sizesView").text("Small (Ksh " + size + ")");
 }
 function sizeOrder2() {
    $(".pizzasizes").show(500);
 
-   var size = $("#medium").val();
+   var size = parseInt($("#medium").val());
 
-   $(".sizesView").text(size);
+   $(".sizesView").text("Medium (Ksh " + size + ")");
 }
 function sizeOrder3() {
    $(".pizzasizes").show(500);
 
-   var size = $("#large").val();
+   var size =parseInt( $("#large").val());
 
-   $(".sizesView").text(size);
+   $(".sizesView").text("Large (Ksh " + size + ")");
 }
+
 
 function crustOrder1() {
    $(".pizzacrusts").show(500);
 
-   var crusts = $("#crispy").val();
+   var crusts = parseInt($("#crispy").val());
 
-   $(".crustsView").append(crusts+ "<br>");
+   $(".crustsView").append("Crispy Crust (Ksh " + crusts + ")<br>");
 }
 function crustOrder2() {
    $(".pizzacrusts").show(500);
 
-   var crusts = $("#stuffed").val();
+   var crusts = parseInt($("#stuffed").val());
 
-   $(".crustsView").append(crusts+ "<br>");
+   $(".crustsView").append("Stufed Crust (Ksh " + crusts + ")<br>");
 }
 function crustOrder3() {
    $(".pizzacrusts").show(500);
 
-   var crusts = $("#gluttenfree").val();
+   var crusts = parseInt($("#gluttenfree").val());
 
-   $(".crustsView").append(crusts+ "<br>");
+   $(".crustsView").append("Glutten Free (Ksh " + crusts + ")<br>");
 }
 
 
 function toppingsOrder1() {
+
    $(".pizzatoppings").show(500);
 
    var toppings = $("#tomatoes").val();
+   allToppings.push(toppings);
 
-   $(".toppingsView").append(toppings+ "<br>");
+   $(".toppingsView").append("Tomatoes (Ksh "+  toppings + ")<br>");
 }
 function toppingsOrder2() {
    $(".pizzatoppings").show(500);
 
    var toppings = $("#cheese").val();
+   allToppings.push(toppings);
 
-   $(".toppingsView").append(toppings+ "<br>");
+   $(".toppingsView").append("Cheese (Ksh "+  toppings + ")<br>");
 }
 function toppingsOrder3() {
    $(".pizzatoppings").show(500);
 
    var toppings = $("#pineapples").val();
+   allToppings.push(toppings);
 
-   $(".toppingsView").append(toppings+ "<br>");
+   $(".toppingsView").append("Pineapples (Ksh "+  toppings + ")<br>");
 }
 function toppingsOrder4() {
    $(".pizzatoppings").show(500);
 
    var toppings = $("#greenPepper").val();
+   allToppings.push(toppings);
 
-   $(".toppingsView").append(toppings+ "<br>");
+   $(".toppingsView").append("Green Pepper (Ksh "+  toppings + ")<br>");
 }
 function toppingsOrder5() {
    $(".pizzatoppings").show(500);
+   allToppings.push(toppings);
 
    var toppings = $("#pepperoni").val();
 
-   $(".toppingsView").append(toppings+ "<br>");
+   $(".toppingsView").append("Pepperoni (Ksh "+  toppings + ")<br>");
 }
 function toppingsOrder6() {
    $(".pizzatoppings").show(500);
 
    var toppings = $("#mushrooms").val();
+   allToppings.push(toppings);
 
-   $(".toppingsView").append(toppings+ "<br>");
+   $(".toppingsView").append("Mushrooms (Ksh "+  toppings + ")<br>");
+}
+
+function addAnother() {
+   var pizzas = parseInt($("#orders").val()); 
+
+   
 }
